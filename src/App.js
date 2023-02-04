2cd90eb6e899fd9c84f08d22b8e2bd8f964eca5f
lizdmukami <lizdmukami@gmail.com>
2023-02-04 14:21:07 +0300

    Add products component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import AddProduct from "./components/AddProduct/AddProduct";
 import Order from "./components/Order/Order";
 import Users from "./components/Users/Users";
+import Product from "./components/Products/Product";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Product />} />
           <Route path="/addproduct" element={<AddProduct />} />
           <Route path="/order" element={<Order />} />
           <Route path="/users" element={<Users />} />
